cdd2524d0ce61bb67cf34ba9ef2566718003076a
Daniel Schaefer <daniel.schaefer@hpe.com>
2020-06-03 15:26:20 +0200

    RISC-V/PlatformPkg: Fixup FDT from HOB and install into config table
    The Linux EFISTUB reads the FDT from the EFI system configuration
    table. Before installing the FDT needs to be patched with the booting
    hartid, because the kernel in S-Mode cannot determine it.
    
    Signed-off-by: Daniel Schaefer <daniel.schaefer@hpe.com>

--- Silicon/RISC-V/ProcessorPkg/Universal/FdtDxe/FdtDxe.c
+++ Silicon/RISC-V/ProcessorPkg/Universal/FdtDxe/FdtDxe.c
@@ -21,15 +21,15 @@
 /**
   Fix up the device tree with booting hartid for the kernel
 
-  @param DtbBlob The device tree. Is extended to fit the hart id.
+  @param FdtBlob The device tree. Is extended to fit the hart id.
 
   @retval EFI_SUCCESS           The device tree was success fixed up with the hart id.
   @retval EFI_OUT_OF_RESOURCES  There is not enough memory available to complete the operation.
 **/
 EFI_STATUS
 EFIAPI
-FixDtb (
-  IN OUT VOID  *DtbBlob,
+FixFdt (
+  IN OUT VOID  *FdtBlob,
   IN     UINTN  BootingHartId
   )
 {
@@ -39,15 +39,15 @@ FixDtb (
   DEBUG ((DEBUG_INFO, "Fixing up device tree with boot hart id: %d\n",
     BootingHartId));
 
-  Size = fdt_totalsize(DtbBlob);
-  Err  = fdt_open_into(DtbBlob, DtbBlob, Size + 32);
+  Size = fdt_totalsize(FdtBlob);
+  Err  = fdt_open_into(FdtBlob, FdtBlob, Size + 32);
   if (Err < 0) {
     DEBUG ((DEBUG_ERROR,
       "Device Tree can't be expanded to accommodate new node\n", __FUNCTION__));
     return EFI_OUT_OF_RESOURCES;
   }
-  ChosenOffset = fdt_path_offset(DtbBlob, "/chosen");
-  fdt_setprop_u32(DtbBlob, ChosenOffset, "boot-hartid", BootingHartId);
+  ChosenOffset = fdt_path_offset(FdtBlob, "/chosen");
+  fdt_setprop_u32(FdtBlob, ChosenOffset, "boot-hartid", BootingHartId);
 
   return EFI_SUCCESS;
 }
@@ -68,16 +68,16 @@ InstallFdtFromHob (VOID)
   VOID              *DataInHob;
   UINTN              DataSize;
 
-  GuidHob = GetFirstGuidHob (&gRiscVDtbHobGuid);
+  GuidHob = GetFirstGuidHob (&gRiscVFlattenedDeviceTreeHobGuid);
   if (GuidHob == NULL) {
-    DEBUG ((DEBUG_ERROR, "Failed to find RISC-V DTB Hob\n",
+    DEBUG ((DEBUG_ERROR, "Failed to find RISC-V FDT Hob\n",
       __FUNCTION__));
     return EFI_NOT_FOUND;
   }
   DataInHob = GET_GUID_HOB_DATA (GuidHob);
   DataSize  = GET_GUID_HOB_DATA_SIZE (GuidHob);
 
-  Status = FixDtb (DataInHob, PcdGet32(PcdBootHartId));
+  Status = FixFdt (DataInHob, PcdGet32(PcdBootHartId));
   if (EFI_ERROR (Status)) {
     return Status;
   }
