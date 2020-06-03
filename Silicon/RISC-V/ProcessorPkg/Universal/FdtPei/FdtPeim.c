83bfda20aebdbdc7192bbe6103674e1e099ae24b
Daniel Schaefer <daniel.schaefer@hpe.com>
2020-06-03 15:30:32 +0200

    RISC-V/PlatformPkg: Pass DTB from PEI to DXE via HOB
    Signed-off-by: Daniel Schaefer <daniel.schaefer@hpe.com>

---
+++ Silicon/RISC-V/ProcessorPkg/Universal/FdtPei/FdtPeim.c
@@ -0,0 +1,67 @@
+/** @file
+The module to pass the device tree to DXE via HOB.
+
+Copyright (c) 2020, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <libfdt.h>
+
+/**
+  TODO
+**/
+EFI_STATUS
+EFIAPI
+BuildFdtHob (
+  IN  UINT64 FdtPtr
+  )
+{
+  VOID *HobData;
+
+  if (fdt_check_header ((VOID*)FdtPtr) != 0) {
+    DEBUG ((DEBUG_INFO, "FDT header is invalid\n"));
+    return EFI_LOAD_ERROR;
+  }
+
+  DEBUG ((DEBUG_INFO, "Installing FDT Hob with length: %d\n", fdt_totalsize(FdtPtr)));
+  HobData = BuildGuidDataHob (
+              &gRiscVFlattenedDeviceTreeHobGuid,
+              (VOID *)FdtPtr,
+              fdt_totalsize(FdtPtr)
+              );
+  if (HobData == NULL) {
+    return EFI_LOAD_ERROR;
+  }
+
+  return EFI_SUCCESS;
+}
+
+/**
+  The entrypoint of the module, it will pass the FDT via a HOB.
+
+  @param  FileHandle             Handle of the file being invoked.
+  @param  PeiServices            Describes the list of possible PEI Services.
+
+  @retval TODO
+**/
+EFI_STATUS
+EFIAPI
+PeimPassFdt (
+  IN EFI_PEI_FILE_HANDLE        FileHandle,
+  IN CONST EFI_PEI_SERVICES     **PeiServices
+  )
+{
+  EFI_STATUS                          Status;
+  UINTN                               ThisHartid;
+  EFI_RISCV_OPENSBI_FIRMWARE_CONTEXT *FirmwareContext;
+
+  Status = SbiGetFirmwareContext (&FirmwareContext);
+  ASSERT_EFI_ERROR (Status);
+
+  Status = BuildFdtHob (FirmwareContext->FlattenedDeviceTree);
+  ASSERT_EFI_ERROR (Status);
+
+	return EFI_SUCCESS;
+}
